23a8d5fc629b3f8c7418264da75a2604d4f7679d
Rohan Kanti <rohan.kanti@mail.mcgill.ca>
2025-08-16 16:26:15 -0400

    gud policy run

--- reinforce.py
+++ reinforce.py
@@ -37,98 +37,100 @@ class ReinforcePolicy(torch.nn.Module):
     # HOW CAN YOU MAKE IT FASTER??
     # WHEN DOES IT START MAKING SENSE TO USE A GPU?
 
-saved_policy_filename = "policy.pth"
-serious_training_run = True
 
-torch.manual_seed(27)
-#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-env_render = gym.make('CartPole-v1', render_mode="human")
-env_headless = gym.make('CartPole-v1')
-env = env_headless
+if __name__ == "__main__":
+    saved_policy_filename = "policy.pth"
+    serious_training_run = True
 
-in_dim = env.observation_space.shape[0] # (4,)
-out_dim = env.action_space.n # Discrete action space, (2,) possible actions for CartPole env
-hidden_dim = 32
-policy = ReinforcePolicy(in_dim,hidden_dim,out_dim)
-#policy.to(device=device)
+    torch.manual_seed(27)
+    #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    env_render = gym.make('CartPole-v1', render_mode="human")
+    env_headless = gym.make('CartPole-v1')
+    env = env_headless
 
-max_iters = 100
-batch_size = 32
-lr = 3e-2
-optimizer = torch.optim.AdamW(policy.parameters(), lr=lr)
+    in_dim = env.observation_space.shape[0] # (4,)
+    out_dim = env.action_space.n # Discrete action space, (2,) possible actions for CartPole env
+    hidden_dim = 32
+    policy = ReinforcePolicy(in_dim,hidden_dim,out_dim)
+    #policy.to(device=device)
 
-start_time = time.perf_counter()
+    max_iters = 100
+    batch_size = 32
+    lr = 3e-2
+    optimizer = torch.optim.AdamW(policy.parameters(), lr=lr)
 
-print("\nstart training...")
+    start_time = time.perf_counter()
 
-# training loop
-avg_reward = []
-avg_len = []
-for batch in range(max_iters):
+    print("\nstart training...")
 
-    batch_acts, batch_states, batch_weights, batch_lens = [], [], [], []
+    # training loop
+    avg_reward = []
+    avg_len = []
+    for batch in range(max_iters):
 
-    #env = env_render if batch % 10 == 0 else env_headless
+        batch_acts, batch_states, batch_weights, batch_lens = [], [], [], []
 
-    for episode in range(batch_size):
-        state,info = env.reset()
-        done = False
-        rews_buf = []
+        #env = env_render if batch % 10 == 0 else env_headless
 
-        while True:
-            batch_states.append(state.tolist())
-            action = policy.get_action(torch.as_tensor(state,dtype=torch.float32))
-            batch_acts.append(action)
-            next_state,reward,terminated,truncated,_ = env.step(action)
-            rews_buf.append(reward)
-            
-            if terminated or truncated:
-                rtgs = policy.rtg(rews_buf)
-                batch_weights.extend(rtgs)
-                batch_lens.append(len(rews_buf))  
-                break
-            else:
-                state = next_state
+        for episode in range(batch_size):
+            state,info = env.reset()
+            done = False
+            rews_buf = []
 
-    batch_avg_reward = sum(batch_weights)/len(batch_weights)
-    batch_avg_len = sum(batch_lens)/len(batch_lens)
-    avg_reward.append(batch_avg_reward) # use to plot later
-    avg_len.append(batch_avg_len)
-    if batch % 10 == 0:
-        print(f"Batch {batch} avg reward: {batch_avg_reward}")
-        print(f"Batch {batch} avg len: {batch_avg_len}")
-    
-    surrogate_loss = policy.surrogate_loss(
-        torch.as_tensor(batch_acts,dtype=torch.float32),
-        torch.as_tensor(batch_states,dtype=torch.float32),
-        torch.as_tensor(batch_weights,dtype=torch.float32)
-        )
-    
-    optimizer.zero_grad()
-    surrogate_loss.backward()
-    optimizer.step()
-            
-env_render.close()
-env_headless.close()
+            while True:
+                batch_states.append(state.tolist())
+                action = policy.get_action(torch.as_tensor(state,dtype=torch.float32))
+                batch_acts.append(action)
+                next_state,reward,terminated,truncated,_ = env.step(action)
+                rews_buf.append(reward)
+                
+                if terminated or truncated:
+                    rtgs = policy.rtg(rews_buf)
+                    batch_weights.extend(rtgs)
+                    batch_lens.append(len(rews_buf))  
+                    break
+                else:
+                    state = next_state
 
-end_time = time.perf_counter()
-train_duration = end_time - start_time
-print(f"Training time (mins): {train_duration/60}")
-print(f"Training time (secs): {train_duration}")
+        batch_avg_reward = sum(batch_weights)/len(batch_weights)
+        batch_avg_len = sum(batch_lens)/len(batch_lens)
+        avg_reward.append(batch_avg_reward) # use to plot later
+        avg_len.append(batch_avg_len)
+        if batch % 10 == 0:
+            print(f"Batch {batch} avg reward: {batch_avg_reward}")
+            print(f"Batch {batch} avg len: {batch_avg_len}")
+        
+        surrogate_loss = policy.surrogate_loss(
+            torch.as_tensor(batch_acts,dtype=torch.float32),
+            torch.as_tensor(batch_states,dtype=torch.float32),
+            torch.as_tensor(batch_weights,dtype=torch.float32)
+            )
+        
+        optimizer.zero_grad()
+        surrogate_loss.backward()
+        optimizer.step()
+                
+    env_render.close()
+    env_headless.close()
 
-if serious_training_run:
-    torch.save(policy.state_dict(),saved_policy_filename)
+    end_time = time.perf_counter()
+    train_duration = end_time - start_time
+    print(f"Training time (mins): {train_duration/60}")
+    print(f"Training time (secs): {train_duration}")
 
-plt.plot([i for i in range(1,max_iters+1)], avg_reward) 
-plt.xlabel("Batch")  
-plt.ylabel("Average Reward")  
-plt.title("Agent Reward")
-plt.show()        
-plt.savefig('batch_avg_reward.png')
+    if serious_training_run:
+        torch.save(policy.state_dict(),saved_policy_filename)
 
-plt.plot([i for i in range(1,max_iters+1)], avg_len) 
-plt.xlabel("Batch")  
-plt.ylabel("Average Len")  
-plt.title("Episode Lengths")
-plt.show()  
-plt.savefig('batch_avg_len.png')     
+    plt.plot([i for i in range(1,max_iters+1)], avg_reward) 
+    plt.xlabel("Batch")  
+    plt.ylabel("Average Reward")  
+    plt.title("Agent Reward")
+    plt.savefig('batch_avg_reward.png')
+    plt.show()        
+
+    plt.plot([i for i in range(1,max_iters+1)], avg_len) 
+    plt.xlabel("Batch")  
+    plt.ylabel("Average Len")  
+    plt.title("Episode Lengths")
+    plt.savefig('batch_avg_len.png')     
+    plt.show()  
--- test.py
+++ test.py
@@ -3,7 +3,10 @@ import gymnasium as gym
 import torch
 
 env = gym.make('CartPole-v1', render_mode="human")
-policy = ReinforcePolicy()
+in_dim = env.observation_space.shape[0] # (4,)
+out_dim = env.action_space.n # Discrete action space, (2,) possible actions for CartPole env
+hidden_dim = 32
+policy = ReinforcePolicy(in_dim,hidden_dim,out_dim)
 policy.load_state_dict(torch.load("policy.pth",weights_only=True))
 
 state,info = env.reset()
@@ -12,6 +15,9 @@ done = False
 while True:
     action = policy.get_action(torch.as_tensor(state,dtype=torch.float32))
     next_state,reward,terminated,truncated,_ = env.step(action)
+
+    if terminated: print("Terminated")
+    elif truncated: print("Truncated")
     
     if terminated or truncated:
         break
