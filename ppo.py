55449a4dadfc775367e24e713ec06c63e78ef955
Rohan Kanti <rohan.kanti@mail.mcgill.ca>
2025-08-16 19:34:28 -0400

    starting ppo, implementing GAE

---
+++ ppo.py
@@ -0,0 +1,200 @@
+import gymnasium as gym
+import torch
+import time
+import matplotlib.pyplot as plt
+
+class PPOPolicy(torch.nn.Module):
+    def __init__(self, in_dim:int, hidden_dim:int, out_dim:int):
+        super().__init__()
+        
+        self.ffnn = torch.nn.Sequential(
+            torch.nn.Linear(in_dim,hidden_dim),
+            torch.nn.ReLU(),
+            torch.nn.Linear(hidden_dim,out_dim),
+        )
+
+    def get_policy(self,state):
+        logits = self.ffnn(state)
+        prob_dist = torch.distributions.Categorical(logits=logits)
+        return prob_dist
+
+    def get_action(self,state):
+        policy = self.get_policy(state)
+        act = policy.sample()
+        return act.item()
+    
+    def rtg(self,rews_buf):
+        rtg_buf = [0]*len(rews_buf)
+        for i in reversed(range(len(rtg_buf))):
+            rtg_buf[i] = rews_buf[i] + (rtg_buf[i+1] if i+1 < len(rtg_buf) else 0)
+        return rtg_buf
+    
+    def surrogate_loss(self,batch_acts,batch_states,batch_weights):
+        #print(f"Batch acts: {batch_acts.shape} ")
+        #print(f"Batch states: {batch_states.shape} ")
+        #print(f"Batch weights: {batch_weights.shape} ")
+
+        logp = self.get_policy(batch_states).log_prob(batch_acts)
+        return -(logp * batch_weights).mean()
+    
+
+class ValueFunction(torch.nn.Module):
+    def __init__(self, in_dim:int, hidden_dim:int, out_dim:int):
+        super().__init__()
+
+        self.ffnn = torch.nn.Sequential(
+            torch.nn.Linear(in_dim,hidden_dim),
+            torch.nn.ReLU(),
+            torch.nn.Linear(hidden_dim,out_dim),
+        )
+
+    def get_value(self,state):
+        logit = self.ffnn(state)
+        return logit.item()
+    
+    def loss_fn(self,batch_states,batch_rtgs):
+        preds = self.ffnn(batch_states)
+        preds = preds.squeeze(-1)
+
+        #print(f"Preds: {preds.shape}")
+        #print(f"Batch rtgs: {batch_rtgs.shape} ")
+
+        return torch.nn.functional.mse_loss(preds,batch_rtgs)
+    
+def GAE(value_fn,reward,state,next_state,lambda_param,gamma_param,l):
+    td = reward + value_fn.get_value(next_state) - value_fn.get_value(state)
+    return ((lambda_param*gamma_param) ** l) * td
+
+    
+if __name__ == "__main__":
+    saved_policy_filename = "ppo_policy.pth"
+    saved_valuefn_filename = "ppo_value.pth"
+    serious_training_run = False
+
+    torch.manual_seed(27)
+    env_render = gym.make('CartPole-v1', render_mode="human")
+    env_headless = gym.make('CartPole-v1')
+    env = env_headless
+
+    in_dim = env.observation_space.shape[0] # (4,)
+    out_dim = env.action_space.n # (2,)
+    hidden_dim = 32
+    policy = PPOPolicy(in_dim,hidden_dim,out_dim)
+    value_fn = ValueFunction(in_dim,hidden_dim,1)
+    
+    max_iters = 2 #100
+    batch_size = 4 #32
+    lr = 3e-2
+    gamma_param = 0.96
+    lambda_param = 0.92
+    optimizer_policy = torch.optim.AdamW(policy.parameters(), lr=lr)
+    optimizer_value_fn = torch.optim.AdamW(value_fn.parameters(), lr=lr)
+
+    start_time = time.perf_counter()
+
+    print("\nstart training...")
+
+    # training loop
+    avg_advantage = []
+    avg_len = []
+    value_fn_loss = []
+
+    for batch in range(max_iters):
+
+        batch_acts, batch_states, batch_weights, batch_lens, batch_rtgs = [], [], [], [], []
+
+        for episode in range(batch_size):
+            state,info = env.reset()
+            done = False
+            rews_buf = []
+            gae_buf = []
+            t = 0
+
+            while True:
+                t += 1
+                batch_states.append(state.tolist())
+                action = policy.get_action(torch.as_tensor(state,dtype=torch.float32))
+                batch_acts.append(action)
+                next_state,reward,terminated,truncated,_ = env.step(action)
+                rews_buf.append(reward)
+                gae = GAE(value_fn,
+                          reward,
+                          torch.as_tensor(state,dtype=torch.float32),
+                          torch.as_tensor(next_state,dtype=torch.float32),
+                          lambda_param,
+                          gamma_param,
+                          t
+                        )
+                gae_buf.append(gae)
+                
+                if terminated or truncated:
+                    rtgs = policy.rtg(rews_buf)
+                    batch_rtgs.extend(rtgs) 
+                    batch_weights.extend(gae_buf)
+                    batch_lens.append(len(rews_buf))  
+                    break
+                else:
+                    state = next_state
+
+        batch_avg_advantage = sum(batch_weights)/len(batch_weights)
+        batch_avg_len = sum(batch_lens)/len(batch_lens)
+        avg_advantage.append(batch_avg_advantage) 
+        avg_len.append(batch_avg_len)
+
+        if batch % 10 == 0:
+            print(f"Batch {batch} avg adv: {batch_avg_advantage}")
+            print(f"Batch {batch} avg len: {batch_avg_len}")
+        
+        surrogate_loss = policy.surrogate_loss(
+            torch.as_tensor(batch_acts,dtype=torch.float32),
+            torch.as_tensor(batch_states,dtype=torch.float32),
+            torch.as_tensor(batch_weights,dtype=torch.float32)
+            )
+        
+        value_loss = value_fn.loss_fn(
+            torch.as_tensor(batch_states,dtype=torch.float32),
+            torch.as_tensor(batch_rtgs,dtype=torch.float32)
+            )
+        
+        optimizer_policy.zero_grad()
+        surrogate_loss.backward()
+        optimizer_policy.step()
+
+        optimizer_value_fn.zero_grad()
+        value_loss.backward()
+        optimizer_value_fn.step()
+
+        value_fn_loss.append(value_loss.item())
+                
+    env_render.close()
+    env_headless.close()
+
+    end_time = time.perf_counter()
+    train_duration = end_time - start_time
+    print(f"Training time (mins): {train_duration/60}")
+    print(f"Training time (secs): {train_duration}")
+
+    if serious_training_run:
+        torch.save(policy.state_dict(),saved_policy_filename)
+        torch.save(value_fn.state_dict(),saved_valuefn_filename)
+
+    plt.plot([i for i in range(1,max_iters+1)], avg_advantage) 
+    plt.xlabel("Batch")  
+    plt.ylabel("Average Advantage")  
+    plt.title("Agent Advantage")
+    plt.savefig('ppo_batch_avg_advantage.png')
+    plt.show()        
+
+    plt.plot([i for i in range(1,max_iters+1)], avg_len) 
+    plt.xlabel("Batch")  
+    plt.ylabel("Average Len")  
+    plt.title("Episode Lengths")
+    plt.savefig('ppo_batch_avg_len.png')     
+    plt.show()  
+
+    plt.plot([i for i in range(1,max_iters+1)], value_fn_loss) 
+    plt.xlabel("Batch")  
+    plt.ylabel("MSE Loss")  
+    plt.title("Value Function Loss")
+    plt.savefig('ppo_value_fn_loss.png')     
+    plt.show() 
