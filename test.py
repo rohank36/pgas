6bc2bd3f07bf096b7c7bd53cc8d2a4702332cd9c
Rohan Kanti <rohan.kanti@mail.mcgill.ca>
2025-08-17 23:30:26 -0400

    added ppo

--- test.py
+++ test.py
@@ -1,17 +1,19 @@
 #from reinforce import ReinforcePolicy
-from reinforce_adv import ReinforcePolicy
+#from reinforce_adv import ReinforcePolicy
+from ppo import Policy
 import gymnasium as gym
 import torch
 
 #policy_filepath = "reinforce/reinforce_policy_rtg.pth"
-policy_filepath = "reinforce_gae/reinforce_gae_policy.pth"
+#policy_filepath = "reinforce_gae/reinforce_gae_policy.pth"
+policy_filepath = "ppo_gae/ppo_gae_policy.pth"
 
 env = gym.make('CartPole-v1', render_mode="human")
 
 in_dim = env.observation_space.shape[0] # (4,)
 out_dim = env.action_space.n # Discrete action space, (2,) possible actions for CartPole env
 hidden_dim = 32
-policy = ReinforcePolicy(in_dim,hidden_dim,out_dim)
+policy = Policy(in_dim,hidden_dim,out_dim)
 
 policy.load_state_dict(torch.load(policy_filepath,weights_only=True))
 
