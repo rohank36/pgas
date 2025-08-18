46852c65f797b797ce6d22f05cdbbfc58aca9d63
Rohan Kanti <rohan.kanti@mail.mcgill.ca>
2025-08-17 21:25:48 -0400

    start ppo

--- ppo.py
+++ ppo.py
@@ -3,11 +3,10 @@ import torch
 import time
 import matplotlib.pyplot as plt
 
-class PPOPolicy(torch.nn.Module):
+class Policy(torch.nn.Module):
     def __init__(self, in_dim:int, hidden_dim:int, out_dim:int):
         super().__init__()
-        self.epsilon = 0.2
-
+        
         self.ffnn = torch.nn.Sequential(
             torch.nn.Linear(in_dim,hidden_dim),
             torch.nn.ReLU(),
@@ -24,25 +23,18 @@ class PPOPolicy(torch.nn.Module):
         act = policy.sample()
         return act.item()
     
-    
-    def rtg(self,rews_buf):
+    def rtg(self,rews_buf,gam=0.99):
         rtg_buf = [0]*len(rews_buf)
         for i in reversed(range(len(rtg_buf))):
-            rtg_buf[i] = rews_buf[i] + (rtg_buf[i+1] if i+1 < len(rtg_buf) else 0)
+            rtg_buf[i] = rews_buf[i] + (gam*rtg_buf[i+1] if i+1 < len(rtg_buf) else 0)
         return rtg_buf
     
     def surrogate_loss(self,batch_acts,batch_states,batch_weights):
-        #print(f"Batch acts: {batch_acts.shape} ")
-        #print(f"Batch states: {batch_states.shape} ")
-        #print(f"Batch weights: {batch_weights.shape} ")
-
-
-
         logp = self.get_policy(batch_states).log_prob(batch_acts)
         return -(logp * batch_weights).mean()
     
 
-class ValueFunction(torch.nn.Module):
+class Value(torch.nn.Module):
     def __init__(self, in_dim:int, hidden_dim:int, out_dim:int):
         super().__init__()
 
@@ -59,168 +51,163 @@ class ValueFunction(torch.nn.Module):
     def loss_fn(self,batch_states,targets):
         preds = self.ffnn(batch_states)
         preds = preds.squeeze(-1)
+        
+        if torch.var(targets)<1e-8:
+            ev = 0
+        else:
+            ev = 1 - (torch.var(targets-preds)/torch.var(targets))
+ 
+        return torch.nn.functional.huber_loss(preds,targets), ev.item()
 
-        #print(f"Preds: {preds.shape}")
-        #print(f"Targets: {targets.shape} ")
-        #print("\n")
-
-        return torch.nn.functional.mse_loss(preds,targets)
-    
-def GAE(value_fn,rewards,states,next_states,lam,gam,terminated):
-    # TD_t = rew[t] + gam*V(s_t+1)*(1-done_t) - V(s_t)
-    # A_t = TD_t + gam*lam*A_t+1*(1-done_t)
-
+def GAE(V,rewards,states,last,lam,gam,terminated):
     with torch.no_grad():
-        v_s = value_fn.get_values(states)
-        v_last = torch.tensor(0.0) if terminated else value_fn.get_values(next_states[-1]) # if terminated last value should be 0 else bootstrap
-        v_full = torch.cat([v_s, v_last.unsqueeze(0)], dim=0) # +1
+        v_s = V.get_values(states) # V(s)
+        v_last = torch.tensor(0.0) if terminated else V.get_values(last) # if terminated last value should be 0 else bootstrap
+        v_next = torch.cat((v_s[1:], v_last.unsqueeze(0))) # V(s_t+1)
 
     dones = torch.zeros(len(rewards), dtype=torch.float32)
     if terminated:
         dones[-1] = 1.0
     not_done = 1.0 - dones
 
-
+    td = rewards + gam * v_next * not_done - v_s
     adv = torch.zeros_like(rewards)
     gae = 0.0
 
     for t in reversed(range(len(rewards))):
-        td = rewards[t] + gam * v_full[t+1] * not_done[t] - v_full[t]
-        gae   = td + gam * lam * not_done[t] * gae
+        gae = td[t] + gam * lam * not_done[t] * gae
         adv[t] = gae
 
-    targets = adv + v_full[:-1]
-
-    """
-    print(f"T: {len(rewards)}")
-    print(f"V(s): {v_s.shape}")
-    print(f"V full: {v_full.shape}")
-    print(f"advantage type: {type(adv)}")
-    print(f"advantage type[0]: {type(adv[0])}")
-    print(adv)
-    print(f"targets type: {type(targets)}")
-    print(f"targets type[0]: {type(targets[0])}")
-    print(targets)
-    print("\n")
-    """
+    targets = adv + v_s
 
     return adv.tolist(),targets.tolist()
-
     
+
 if __name__ == "__main__":
-    saved_policy_filename = "ppo_policy.pth"
-    saved_valuefn_filename = "ppo_value.pth"
+    saved_policy_filename = "ppo_gae_policy.pth"
     serious_training_run = False
 
     torch.manual_seed(27)
+    #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     env_render = gym.make('CartPole-v1', render_mode="human")
     env_headless = gym.make('CartPole-v1')
     env = env_headless
 
     in_dim = env.observation_space.shape[0] # (4,)
-    out_dim = env.action_space.n # (2,)
+    out_dim = env.action_space.n # Discrete action space, (2,) possible actions for CartPole env
     hidden_dim = 32
-    policy = PPOPolicy(in_dim,hidden_dim,out_dim)
-    value_fn = ValueFunction(in_dim,hidden_dim,1)
-    
-    max_iters = 100
-    batch_size = 32 #32
-    policy_lr = 3e-4
-    value_lr  = 3e-3
+    policy = Policy(in_dim,hidden_dim,out_dim)
+    value = Value(in_dim,hidden_dim,1)
+    #policy.to(device=device)
+
     gam = 0.99
     lam = 0.95
+    max_iters = 71
+    batch_size = 32
+    policy_lr = 3e-2
+    value_lr = 3e-3
     optimizer_policy = torch.optim.AdamW(policy.parameters(), lr=policy_lr)
-    optimizer_value_fn = torch.optim.AdamW(value_fn.parameters(), lr=value_lr)
+    optimizer_value = torch.optim.AdamW(value.parameters(), lr=value_lr, weight_decay=0.0)
 
     start_time = time.perf_counter()
 
     print("\nstart training...")
 
     # training loop
-    avg_advantage = []
     avg_len = []
-    value_fn_loss = []
-
+    batch_value_loss = []
+    ev_vals = []
+    epochs = 20
     for batch in range(max_iters):
 
         batch_acts, batch_states, batch_weights, batch_lens, batch_targets = [], [], [], [], []
+
+        #env = env_render if batch % 10 == 0 else env_headless
 
         for episode in range(batch_size):
             state,info = env.reset()
             done = False
             rews_buf = []
             states_buf = []
-            next_states_buf = []
-            t = 0
 
             while True:
-                t += 1
                 batch_states.append(state.tolist())
                 states_buf.append(state.tolist())
                 action = policy.get_action(torch.as_tensor(state,dtype=torch.float32))
                 batch_acts.append(action)
                 next_state,reward,terminated,truncated,_ = env.step(action)
-                next_states_buf.append(next_state.tolist())
                 rews_buf.append(reward)
                 
                 if terminated or truncated:
-                    rtgs = policy.rtg(rews_buf)
                     """
+                    # for adv = rtg - V(s)
+                    rtgs = policy.rtg(rews_buf,gam)
+                    baseline = value.get_values(torch.as_tensor(states_buf,dtype=torch.float32))
+                    advantage = (torch.as_tensor(rtgs,dtype=torch.float32) - baseline)
+                    batch_weights.extend(advantage.tolist())
+                    batch_targets.extend(rtgs)
+                    """
+                    # for GAE
                     advs,targets = GAE(
-                        value_fn=value_fn,
-                        rewards = torch.as_tensor(rews_buf, dtype=torch.float32),
-                        states=torch.as_tensor(states_buf,dtype=torch.float32),
-                        next_states=torch.as_tensor(next_states_buf,dtype=torch.float32),
-                        lam=lam,
-                        gam=gam,
-                        terminated=terminated
+                        value,
+                        torch.as_tensor(rews_buf,dtype=torch.float32),
+                        torch.as_tensor(states_buf,dtype=torch.float32),
+                        torch.as_tensor(next_state,dtype=torch.float32),
+                        lam,
+                        gam,
+                        terminated
                     )
-                    """
-                    batch_weights.extend(rtgs)
-                    #batch_weights.extend((torch.as_tensor(rtgs,dtype=torch.float32) - value_fn.get_values(torch.as_tensor(states_buf,dtype=torch.float32))).tolist())
-                    #batch_weights.extend(advs)
-                    #batch_targets.extend(targets) 
-                    batch_targets.extend(rtgs) 
+                    batch_weights.extend(advs)
+                    batch_targets.extend(targets)
+                    ###
+
                     batch_lens.append(len(rews_buf))  
                     break
                 else:
                     state = next_state
 
-        batch_avg_advantage = sum(batch_weights)/len(batch_weights)
         batch_avg_len = sum(batch_lens)/len(batch_lens)
-        avg_advantage.append(batch_avg_advantage) 
         avg_len.append(batch_avg_len)
 
-        if batch % 10 == 0:
-            print(f"Batch {batch} avg adv: {batch_avg_advantage}")
-            print(f"Batch {batch} avg len: {batch_avg_len}")
-        
-        # normalize batch weights (advantages)
-        #batch_weights_tensor = torch.as_tensor(batch_weights,dtype=torch.float32)
-        #batch_weights_normd = (batch_weights_tensor - batch_weights_tensor.mean()) / (batch_weights_tensor.std() + 1e-8)
-
+        batch_weights_tensor =  torch.as_tensor(batch_weights,dtype=torch.float32)
+        batch_weights_normd = (batch_weights_tensor - batch_weights_tensor.mean())/(batch_weights_tensor.std() + 1e-8)
+    
         surrogate_loss = policy.surrogate_loss(
             torch.as_tensor(batch_acts,dtype=torch.long),
             torch.as_tensor(batch_states,dtype=torch.float32),
-            torch.as_tensor(batch_weights,dtype=torch.float32)
-            #batch_weights_normd
-            )
-        
-        value_loss = value_fn.loss_fn(
-            torch.as_tensor(batch_states,dtype=torch.float32),
-            torch.as_tensor(batch_targets,dtype=torch.float32)
+            batch_weights_normd
             )
         
         optimizer_policy.zero_grad()
         surrogate_loss.backward()
         optimizer_policy.step()
+        
+        for epoch in range(epochs):
+            evs_in_epochs = []
+            value_loss_in_epochs = []
+            value_loss,ev = value.loss_fn(
+                torch.as_tensor(batch_states,dtype=torch.float32),
+                torch.as_tensor(batch_targets,dtype=torch.float32)
+                )
+            evs_in_epochs.append(ev)
+            value_loss_in_epochs.append(value_loss.item())
 
-        optimizer_value_fn.zero_grad()
-        value_loss.backward()
-        optimizer_value_fn.step()
+            optimizer_value.zero_grad()
+            value_loss.backward()
+            optimizer_value.step()
+        
+        avg_value_loss_in_epochs = sum(value_loss_in_epochs)/epochs
+        avg_ev_in_epochs = sum(evs_in_epochs)/epochs
+        batch_value_loss.append(avg_value_loss_in_epochs)
+        ev_vals.append(avg_ev_in_epochs)
 
-        value_fn_loss.append(value_loss.item())
-                
+        if batch % 10 == 0:
+            print(f"Batch {batch} avg len: {batch_avg_len}")
+            print(f"Batch {batch} value loss: {avg_value_loss_in_epochs}")
+            print(f"Batch {batch} ev: {avg_ev_in_epochs}")
+            print("\n")
+
+
     env_render.close()
     env_headless.close()
 
@@ -231,25 +218,24 @@ if __name__ == "__main__":
 
     if serious_training_run:
         torch.save(policy.state_dict(),saved_policy_filename)
-        torch.save(value_fn.state_dict(),saved_valuefn_filename)
-
-    plt.plot([i for i in range(1,max_iters+1)], avg_advantage) 
-    plt.xlabel("Batch")  
-    plt.ylabel("Average Advantage")  
-    plt.title("Agent Advantage")
-    plt.savefig('ppo_batch_avg_advantage.png')
-    plt.show()        
 
     plt.plot([i for i in range(1,max_iters+1)], avg_len) 
     plt.xlabel("Batch")  
     plt.ylabel("Average Len")  
     plt.title("Episode Lengths")
-    plt.savefig('ppo_batch_avg_len.png')     
+    plt.savefig('batch_avg_len_gae.png')     
     plt.show()  
 
-    plt.plot([i for i in range(1,max_iters+1)], value_fn_loss) 
+    plt.plot([i for i in range(1,max_iters+1)], batch_value_loss) 
     plt.xlabel("Batch")  
-    plt.ylabel("MSE Loss")  
+    plt.ylabel("Loss")  
     plt.title("Value Function Loss")
-    plt.savefig('ppo_value_fn_loss.png')     
+    plt.savefig('value_fn_loss_gae.png')     
+    plt.show() 
+
+    plt.plot([i for i in range(1,(max_iters+1))], ev_vals) 
+    plt.xlabel("Batch")  
+    plt.ylabel("EV")  
+    plt.title("Explained Variance")
+    plt.savefig('ev_gae.png')     
     plt.show() 
