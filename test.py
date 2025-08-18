1d0e226f204c472cf2333e60bf92ec07673a2e85
Rohan Kanti <rohan.kanti@mail.mcgill.ca>
2025-08-17 21:19:38 -0400

    edited test code

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
-from reinforce import ReinforcePolicy
+#from reinforce import ReinforcePolicy
+from reinforce_adv import ReinforcePolicy
 import gymnasium as gym
 import torch
 
-policy_filepath = "reinforce/reinforce_policy_rtg.pth"
+#policy_filepath = "reinforce/reinforce_policy_rtg.pth"
+policy_filepath = "reinforce_gae/reinforce_gae_policy.pth"
 
 env = gym.make('CartPole-v1', render_mode="human")
 
@@ -15,15 +17,18 @@ policy.load_state_dict(torch.load(policy_filepath,weights_only=True))
 
 state,info = env.reset()
 done = False
+step_counter = 0
 
 while True:
     action = policy.get_action(torch.as_tensor(state,dtype=torch.float32))
     next_state,reward,terminated,truncated,_ = env.step(action)
+    step_counter += 1
 
     if terminated: print("Terminated")
     elif truncated: print("Truncated")
     
     if terminated or truncated:
+        print(f"Steps: {step_counter}")
         break
     else:
         state = next_state
